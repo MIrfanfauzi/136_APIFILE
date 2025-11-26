caa4d8a413705448cdfc8b9e1fcd1994a62cd6fa
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-26 11:02:40 +0700

    membuat konfigurasi server yang akan dijalankan

--- config/db.js
+++ config/db.js
@@ -5,11 +5,11 @@ async function connectDatabase() {
         await db.sequelize.authenticate();
         console.log('Database connected successfully.');
 
-        await db.sequelize.sync({ alter: true });
+        await db.sequelize.sync({ alter: false });
         console.log('All models were synchronized successfully.');
 
     } catch (error) {
-        console.error('Database connection failed:', err.message);
+        console.error('Database connection failed:', error.message);
         process.exit(1);
     }
 }
