52f2564744b43f9c665eed580e6f5372690de9ce
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-26 11:20:32 +0700

    membuat endpoint create dan get

---
+++ controllers/komikController.js
@@ -0,0 +1,47 @@
+// controllers/komikController.js
+
+const db = require('../models');
+const komikService = require('../services/komikService');
+
+async function createKomik(req, res) {
+  try {
+    const komikData = req.body;
+
+    // Jika ada file upload, tambahkan data gambar ke komikData
+    if (req.file) {
+      komikData.imageType = req.file.mimetype;
+      komikData.imageName = req.file.originalname;
+      komikData.imageData = req.file.buffer;
+    }
+
+    const result = await komikService.createKomik(db, komikData);
+    res.status(201).json({ success: true, data: result });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+}
+
+async function getAllKomik(req, res) {
+  try {
+    const result = await komikService.getAllKomik(db);
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+}
+
+async function getKomikById(req, res) {
+  try {
+    const { id } = req.params;
+    const result = await komikService.getKomikById(db, id);
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    res.status(404).json({ success: false, error: error.message });
+  }
+}
